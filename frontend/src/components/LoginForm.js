88320b02f2d6e1232d7e70b70cb16568a5cf0c1f
Anitta Antony <20076981@mydbs.ie>
2025-11-18 22:12:13 +0000

    add env variable for api url

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -12,10 +12,16 @@ function LoginForm() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
-        email,
-        password,
-      });
+      // const res = await axios.post("http://localhost:5000/api/auth/login", {
+      //   email,
+      //   password,
+      // });
+      
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/auth/login`,
+            { email, password }
+      );
+
 
       setMessage(res.data.message); 
      
