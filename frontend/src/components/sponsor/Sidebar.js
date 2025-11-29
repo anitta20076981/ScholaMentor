1aa95140a32cda7105feb6508aa0b7667ffdfa82
Anitta Antony <20076981@mydbs.ie>
2025-11-29 18:31:03 +0000

    setup sponsor dashboard

---
+++ frontend/src/components/sponsor/Sidebar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  FaHome,
+  FaUserGraduate,
+  FaHandHoldingUsd,
+  FaUsers,
+  FaChalkboardTeacher,
+} from "react-icons/fa";
+
+import "./Sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2 className="sidebar-title">ScholaMentor</h2>
+
+      <nav className="sidebar-menu">
+        <NavLink to="/sponsor/dashboard" className="sidebar-link">
+          <FaHome className="icon" /> Dashboard
+        </NavLink>
+
+        <NavLink to="/sponsor/students" className="sidebar-link">
+          <FaUserGraduate className="icon" /> Students
+        </NavLink>
+
+        <NavLink to="/sponsor/my-sponsorships" className="sidebar-link">
+          <FaHandHoldingUsd className="icon" /> My Sponsorships
+        </NavLink>
+
+        <NavLink to="/sponsor/donations" className="sidebar-link">
+          <FaUsers className="icon" /> Donations
+        </NavLink>
+
+        <NavLink to="/sponsor/mentorship" className="sidebar-link">
+          <FaChalkboardTeacher className="icon" /> Mentor Panel
+        </NavLink>
+      </nav>
+    </aside>
+  );
+}
