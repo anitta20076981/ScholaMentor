27b2be73330d31ec7c01c455563d44ec3f305590
Anitta Antony <20076981@mydbs.ie>
2025-12-02 12:05:16 +0000

    add all students sponsorship requests

--- backend/controllers/sponsorController.js
+++ backend/controllers/sponsorController.js
@@ -33,6 +33,36 @@ exports.getRecommendedStudents = async (req, res) => {
   }
 };
 
+exports.getAllStudentRequest = async (req, res) => {
+  try {
+    const query = `
+      SELECT 
+      sa.*,
+      u.name AS student_name,
+      u.email AS student_email,
+      u.type AS user_type,
+      sd.course,
+      sd.cgpa AS student_cgpa
+      FROM sponsorshipapplications AS sa
+      JOIN users AS u ON sa.student_id = u.id
+      JOIN studentdetails AS sd ON sd.student_id = u.id
+      ORDER BY sd.cgpa DESC
+
+    `;
+
+    const [rows] = await db.execute(query);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "No sponsorship requests found" });
+    }
+
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch recommended students" });
+  }
+};
+
  
 
 exports.getStudentRequest = async (req, res) => {
