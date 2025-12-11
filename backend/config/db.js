0e50342cb714146105e80dfbc0dda1c55346abc1
Anitta Antony <20076981@mydbs.ie>
2025-12-11 20:05:00 +0000

    correct port for railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -17,7 +17,7 @@ const db = mysql.createPool({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT 
 });
 
 module.exports = db.promise();
