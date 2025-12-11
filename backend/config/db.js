d281ce0d75ce2a7518a97d6205342fcec34ce8c3
Anitta Antony <20076981@mydbs.ie>
2025-12-11 19:33:59 +0000

    Update db.js to use Railway environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,22 @@
+// const mysql = require('mysql2');
+
+// const db = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'scholamentor'
+// });
+
+// module.exports = db.promise();
+
+
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'scholamentor'
+  host: process.env.DB_HOST,       // MySQL host from Railway
+  user: process.env.DB_USER,       // MySQL username from Railway
+  password: process.env.DB_PASSWORD, // MySQL password from Railway
+  database: process.env.DB_NAME     // Database name from Railway
 });
 
 module.exports = db.promise();
